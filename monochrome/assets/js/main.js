354dbf5b90d733836eabdf9ef09899d27989d1fc
naufalhfzhn <raden.naufal123@gmail.com>
2019-01-20 15:22:53 +0700

    add t-scroll effect to next post

--- monochrome/assets/js/main.js
+++ monochrome/assets/js/main.js
@@ -1,6 +1,6 @@
-
 $(document).ready(function() {
 
+	// Set Width of main Nav ==========
 	const mainNav 	= $('.main-nav');
 	mainNav.children().children().each(function (index, li) {
 		const divider	= 100 / mainNav.children().length;
@@ -8,6 +8,8 @@ $(document).ready(function() {
 	})
 	// Set Width of main Nav ==========
 
+
+	// toggle mobile nav ===========
 	$('.side-nav ul').hide();
 
 	$('#toggle-nav').on('click', function() {
@@ -18,6 +20,15 @@ $(document).ready(function() {
 	})
 	// toggle mobile nav ===========
 
+
+	// T-Scroll ====================
+	$('.next-post').children().each(function(index,box) {
+		if (index %2 == 0) {
+			$(box).addClass('fadeLeft');
+		} else {
+			$(box).addClass('fadeRight');
+		}
+	})
 
 	Tu.tScroll({
 		't-element'	: '.fadeLeft'
